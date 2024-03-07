d15bc0273290008c8bdb11343610d87624a1849d
Ben Osborne <ben@osbornesupremacy.com>
2024-03-07 09:06:48 -0600

    deleted .NET API

--- src/villains-lambda-api/src/villains-lambda-api/Usings.cs
+++
@@ -1,5 +0,0 @@
-global using FluentValidation;
-global using Microsoft.AspNetCore.Mvc;
-global using Villains.Library.Abstractions;
-global using Villains.Library.Models;
-global using Villains.Library.Services;
