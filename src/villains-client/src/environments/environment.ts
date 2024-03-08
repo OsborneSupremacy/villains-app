7c62c599e1fcd3b8671ee2f6dfeee4ed99bc77c1
Ben Osborne <ben@osbornesupremacy.com>
2024-03-08 14:52:55 -0600

    added HttpClient

--- src/villains-client/src/environments/environment.ts
+++ src/villains-client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://eujy7vsmkk.execute-api.us-east-1.amazonaws.com/live'
+  apiUrl: 'https://eujy7vsmkk.execute-api.us-east-1.amazonaws.com/live/'
 };
