1d33c0b91e2fdbd9d2e15c616025626dac777a9b
Ben Osborne <ben@osbornesupremacy.com>
2024-03-14 10:56:25 -0500

    fixed name of versioning policy

--- iac/terraform/s3.tf
+++ iac/terraform/s3.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "villains-images" {
   )
 }
 
-resource "aws_s3_bucket_versioning" "versioning_example" {
+resource "aws_s3_bucket_versioning" "villains-images-versioning" {
   bucket = aws_s3_bucket.villains-images.id
   versioning_configuration {
     status = "Disabled"
