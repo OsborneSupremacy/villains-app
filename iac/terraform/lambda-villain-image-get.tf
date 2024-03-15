3ec01495bc3d343d9c483f5468a2a1472dad0e86
Ben Osborne <ben@osbornesupremacy.com>
2024-03-15 14:56:29 -0500

    added image get lambda terraform

---
+++ iac/terraform/lambda-villain-image-get.tf
@@ -0,0 +1,19 @@
+module "lambda-villain-image-get" {
+  source      = "./modules/lambda"
+  common_tags = local.common_tags
+  environment_variables = {
+    "TABLE_NAME"        = aws_dynamodb_table.villains.name
+    "BUCKET_NAME"       = aws_s3_bucket.villains-images.bucket
+    "MAX_PAYLOAD_BYTES" = 6291556
+  }
+  function_description       = "Get a villain's image"
+  function_handler           = "Villains.Lambda.Image.Get::Villains.Lambda.Image.Get.Function::FunctionHandler"
+  function_memory_size       = 2048
+  function_name              = "villain-image-get"
+  function_project_directory = "../../src/Villains.Lambda.Image.Get/src/Villains.Lambda.Image.Get"
+}
+
+resource "aws_iam_role_policy_attachment" "villain-image-get-exec-role-attachment-lambda-execute" {
+  role       = module.lambda-villain-image-get.function_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSLambdaExecute"
+}
