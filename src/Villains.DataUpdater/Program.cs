31665117f00379f9268f4cb5359cf04680f3dcf7
Ben Osborne <ben@osbornesupremacy.com>
2024-03-28 13:11:51 -0500

    Created DataUpdater console app

---
+++ src/Villains.DataUpdater/Program.cs
@@ -0,0 +1,13 @@
+﻿const string message = "Enter [red]confirm[/] to continue upgrade process.";
+
+AnsiConsole.MarkupLine(message);
+
+while (
+    !AnsiConsole
+        .Prompt(new TextPrompt<string>("Input:"))
+        .Equals("confirm", StringComparison.OrdinalIgnoreCase)
+    )
+    AnsiConsole.MarkupLine(message);
+
+
+Console.Write("HELLO WORLD!");
